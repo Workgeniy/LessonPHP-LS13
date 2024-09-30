8aaa498ddd2729d3e0c5c5d0281e582b4048ca71
Fedosov A. <leha.fedosow@gmail.com>
2024-09-30 18:09:20 +0300

    homework_13

---
+++ app/app/Console/Commands/UploadFilesCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Faker\Core\File;
+use FFI;
+use Illuminate\Console\Command;
+use Illuminate\Container\Attributes\Storage;
+
+
+class UploadFilesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:upload-files-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $file = "./app/public/robots.txt"; // image
+
+        if (is_file($file)){
+           Storage::put('robots.txt', File::get($file));
+        }
+    }
+}
